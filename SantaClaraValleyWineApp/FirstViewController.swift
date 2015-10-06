f701084a27427d7a9325d9d2324f038b76d15c4f
Naghmeh <naghmeh@naghmehs-macbook-air.local>
2015-10-06 13:23:50 -0700

    Initial Commit

---
+++ SantaClaraValleyWineApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SantaClaraValleyWineApp
+//
+//  Created by Naghmeh on 10/6/15.
+//  Copyright © 2015 SCVWAA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
