e07f0bfd3c72f74d0a4ad2e4909a8f08b7875f6d
Naghmeh <naghmeh@naghmehs-macbook-air.local>
2015-11-25 11:22:12 -0800

    Associates table view

--- SantaClaraValleyWineApp/FifthViewController.swift
+++ SantaClaraValleyWineApp/FifthViewController.swift
@@ -9,12 +9,35 @@
 
 import UIKit
 
-class FifthViewController: UIViewController {
-    @IBOutlet var textView: UITextView!
+class FifthViewController: UIViewController, UITableViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
 
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        print(listOfAssociates.count)
+        return listOfAssociates.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("associateCell")
+        
+        cell!.textLabel?.text = listOfAssociates[indexPath.row].name
+        cell!.detailTextLabel?.text = listOfAssociates[indexPath.row].address
+        cell!.backgroundColor = UIColor.lightGrayColor()
+        cell!.textLabel?.textColor = UIColor.whiteColor()
+        cell!.detailTextLabel?.textColor = UIColor.whiteColor()
+        
+        return cell!
+    }
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath)->NSIndexPath?
+    {
+        //rowCounter = indexPath.row
+        //print(rowCounter)
+        return indexPath
     }
     
     override func didReceiveMemoryWarning() {
